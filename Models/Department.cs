0de92b4b8a139ca3eaf2e35ca22727b8f1bedef7
efsaneabbas <tu7k3zlya@code.edu.az>
2024-04-02 18:13:03 +0400

    first commit

---
+++ Models/Department.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Department
+    {
+        public int No {  get; set; }
+
+        public string Employees {  get; set; }
+
+        public int EmployeesLimit { get; set; }
+
+        Employee[] Employee = new Employee[] { };
+
+
+
+        public Department()
+        {
+            No++;
+        }
+        public void AddEmployee(Employee employee)
+        {
+            Array.Resize( ref Employee, Employee.Length +1 );
+            Employee[Employee.Length -1] = employee;
+
+        }
+        public Employee GetEmployee(int id)
+        {
+            Employee searchForId = null;
+            for (int i = 0; i < Employee.Length; i++)
+            {
+                if (Employee[i].Id == id)
+                {
+                    searchForId = Employee[i];
+                }
+            }
+            return searchForId;
+        }
+        public void GetEmployeeInfo(Employee employee)
+        {
+            Console.WriteLine($"{employee.Name}\n{employee.Surname}\n{employee.Age} \n{ employee.DepartmentNo}\n {employee.Salary}");
+        }
+        public string GetAllEmployees(Employee employee)
+        {
+            return $"{employee.Name}\n{employee.Surname}\n{employee.Age} \n{employee.DepartmentNo}\n {employee.Salary}";
+
+        }
+         public Employee[] GetAllEMployeesBySalary( int minSalary, int maxSalary)
+         {
+            Employee[] newEmployee = new Employee[] { };
+            for(int i = 0; i < Employee.Length; i++)
+            {
+              if (Employee[i].Salary >= minSalary && Employee[i].Salary  <= maxSalary)
+              {
+                    Array.Resize(ref Employee, Employee.Length + 1);
+                    Employee[Employee.Length-1] = Employee[i];
+              }
+            }
+            return Employee;
+         }
+
+
+    }
+
+}
+
+   
+      
+   
+
+
+
+
+
---
+++ Models/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Employee
+    {
+      
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public int DepartmentNo;
+        private double _salary;
+        public int Id;
+        public double Salary 
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                _salary = value;
+            }
+            
+        }     
+
+        public Employee()
+        {
+            Id++;
+
+        }
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name:{Name}\n" + $"Surname:{Surname}" + $"Age:{Age}" + $"DepartmentNo:{DepartmentNo}" + $"Salary:{Salary}");
+        }
+
+        
+        
+       
+
+    }
+}
